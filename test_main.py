98ccc30f99899e77efe7e15d9f3b315e820e245a
Atul Rawat <atulr830@gmail.com>
2025-10-08 18:53:06 +0530

    again test case

--- test_main.py
+++ test_main.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "FastAPI CI pipeline works!"}
+    assert response.json() == {"Message": "Hello Atul CICD First Chang"}
