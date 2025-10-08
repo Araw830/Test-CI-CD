5fe6dc5cbe6138220935c06374bb064fe8be9f05
Atul Rawat <atulr830@gmail.com>
2025-10-08 19:05:13 +0530

    new function

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"Message": "Hello Atul CICD First Chang"}
+
+
+@app.get("/user/{name}")
+def get_name(name):
+    return {"message": f"Hello my name is {name}"}
